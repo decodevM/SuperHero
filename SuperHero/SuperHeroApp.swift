a55a1b29dd13b45113e5604f5355a6ca3704e10d
DecoDev <decodev.contact@gmail.com>
2023-08-29 20:38:06 +0100

    Initial Commit

---
+++ SuperHero/SuperHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroApp.swift
+//  SuperHero
+//
+//  Created by Inconnu on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
